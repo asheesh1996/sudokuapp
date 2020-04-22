c11ee57cfe80258386625c62a92b8d6701a3f8cd
Asheesh Maheshwari <asheeshagra@gmail.com>
2020-04-22 20:51:11 +0530

    Updates

---
+++ precache-manifest.b3bcc9ecc08d75fc2d299d260f5e266d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b0000b9e6d0d8eea6fc4adb821a599",
+    "url": "/sudokuapp/index.html"
+  },
+  {
+    "revision": "4a0a1c6b847ed330f4f5",
+    "url": "/sudokuapp/static/js/2.cff584c5.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/sudokuapp/static/js/2.cff584c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7df6cb0aa5df7cfe3b9",
+    "url": "/sudokuapp/static/js/main.acd37e94.chunk.js"
+  },
+  {
+    "revision": "3730a6f78684149408ab",
+    "url": "/sudokuapp/static/js/runtime-main.5d73f52c.js"
+  }
+]);
